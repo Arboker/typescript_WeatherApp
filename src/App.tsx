55f69e9d24053340f32f5c4393fe0c85aaa7f3c6
Arboker <denislupookov@gmail.com>
2021-01-20 12:34:24 +0200

    changing url

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ const App: React.FC = () => {
   return (
     <div className="container">
       <Switch>
-        <Route exact path={process.env.PUBLIC_URL+"/"}><Main /></Route>
-        <Route path={process.env.PUBLIC_URL+"/forecast/:city"} children={<Forecast />} />
-        <Route path={process.env.PUBLIC_URL+"/:city/:day"} children={<Weather />} />
+        <Route exact path="typescript_WeatherApp/"><Main /></Route>
+        <Route path={"typescript_WeatherApp"+"/forecast/:city"} children={<Forecast />} />
+        <Route path={"typescript_WeatherApp"+"/:city/:day"} children={<Weather />} />
         <Route component={NotFound} />
       </Switch>
     </div>
